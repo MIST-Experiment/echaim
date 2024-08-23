60a85d89c82a8416d85b1f13a73b3c121b553402
vbidula <vadym.bidula@gmail.com>
2024-08-22 21:37:56 -0400

    density_profile now handles latitudes < 55 deg but returns nans

--- src/echaim/density_profile.py
+++ src/echaim/density_profile.py
@@ -26,15 +26,21 @@ def density_profile(lats: np.ndarray, lons: np.ndarray, alts: np.ndarray, dt: da
         raise ValueError("Array of altitudes must be 1D")
     l1 = c_int(len(alts))
 
-    lats = lats.astype(np.float64)
-    lons = lons.astype(np.float64)
+    edens = np.empty((len(lats), len(alts)))
+    ind_in = np.argwhere(lats >= 55).squeeze()
+    ind_out = np.argwhere(lats < 55).squeeze()
+
+    edens[ind_out, :] = np.nan
+
+    lats_in = lats[ind_in].astype(np.float64)
+    lons_in = lons[ind_in].astype(np.float64)
     alts = alts.astype(np.float64)
 
     alts_p = alts.ctypes.data_as(c_double_p)
-    lats_p, lons_p, l0 = prepare_coords(lats, lons)
+    lats_p, lons_p, l0 = prepare_coords(lats_in, lons_in)
     year_p, month_p, day_p, hour_p, minute_p, second_p = prepare_dt(dt, len(lats))
 
-    output = np.zeros(len(alts)*len(lats), dtype=np.float64)
+    output = np.zeros(len(alts)*len(lats_in), dtype=np.float64)
 
     output_p = output.ctypes.data_as(c_double_p)
 
@@ -44,8 +50,9 @@ def density_profile(lats: np.ndarray, lons: np.ndarray, alts: np.ndarray, dt: da
                                hour_p, minute_p, second_p,
                                c_int(storm), c_int(precip), c_int(dregion),
                                l0, alts_p, l1, c_int(0))
-
-    return output.reshape((len(lats), len(alts)))
+    edens_in = output.reshape((len(lats_in), -1))
+    edens[ind_in] = edens_in
+    return edens
 
 
 def density_path(lats: np.ndarray, lons: np.ndarray, alts: np.ndarray, dt: datetime | Collection, storm: bool = False,
